80882853599c973c89eb4b293857e2687f69a1db
Telsy SpA @ CTI Team <49303361+telsy-cyberops@users.noreply.github.com>
2019-11-29 16:46:31 +0100

    Update nokia_impact_RCE.go

--- PoC/Nokia/nokia_impact_RCE.go
+++ PoC/Nokia/nokia_impact_RCE.go
@@ -2,6 +2,7 @@
 // Chaining CVE-2019-17403, CVE-2019-17404 and CVE -2019-17406 - (Full Path disclosure, Unrestricted File Upload and Path Traversal in /ui/deviceImport leading to post-auth RCE PoC)
 // Reference of Research: https://www.telecomitalia.com/tit/it/innovazione/cybersecurity/red-team.html
 // PoC Author: Francesco Giordano (Telsy SpA)
+
 package main
 
 import (
